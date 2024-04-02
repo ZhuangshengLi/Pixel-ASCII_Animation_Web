c0d6ec90f6c6ffde9be7a390f65e268285f366f9
sherry <1799470570@qq.com>
2024-04-02 17:47:20 +0100

    change variables into global ones

--- MAIN/MAIN.py
+++ MAIN/MAIN.py
@@ -1,59 +1,51 @@
-import os
-
-from parameterList import *
-from function import * #所有操作函数
+import var
+import globals
 from command import *
+from function import *  # 所有操作函数
 
 if __name__ == "__main__":
 
-    # os.system("ffmpeg -i " + ascii_output_path + "%04d.jpg.ascii.png" " -c:v libx264 -vf fps=" + str(frames) + " -pix_fmt yuv420p " + video_output_path)
+    globals.init()
+    var.init()
 
-    # os.system("ffmpeg -i D:/pythonproj/pythonProject/vedio.mp4 -an -r 60 -filter:v \"setpts=2.0*PTS\" D:/pythonproj/pythonProject/normal_vedio.mp4")
-
-
-    #切换到项目目录
+    # 切换到项目目录
     changeToProjectFold()
 
-    #生成帧的暂存目录
+    # 生成帧的暂存目录
     foldGenerate()
 
-    #获取视频宽高
+    # 获取视频宽高
     get_size()
 
-    '''#视频分帧
-    getFrames()
+    # 视频分帧
+    # getFrames()
 
-    all_file = os.listdir(frames_of_original)
-    if(pattern.PATTERN_ASCII == 1):
+    all_file = os.listdir(globals.get_value('frames_of_original'))
 
-       process = [img2ASCIImg(frames_of_original + x) for x in all_file if x.endswith('.png') or x.endswith('.jpg')]
-       # 计算ascii图像的数量
-       para.num_of_ascii = num_of_file(frames_of_ascii)
+    if (globals.get_value('PATTERN_ASCII')):
 
-    elif(pattern.PATTERN_PIXEL == 1):
+        process = [img2ASCIImg(globals.get_value('frames_of_original') + x, globals.get_value('size')) for x in all_file if
+                   x.endswith('.png') or x.endswith('.jpg')]
+        # 计算ascii图像的数量
+        num_of_ascii = num_of_file(globals.get_value('frames_of_ascii'))
+        globals.set_value('num_of_ascii', num_of_ascii)
 
-       process = [img2PIXImg(frames_of_original + x) for x in all_file if x.endswith('.png') or x.endswith('.jpg')]
-       # 计算ascii图像的数量
-       para.num_of_pixel = num_of_file(frames_of_pixel)'''
+    elif (globals.get_value('PATTERN_PIXEL')):
 
+        process = [img2PIXImg(globals.get_value('frames_of_original') + x, globals.get_value('size')) for x in all_file if
+                   x.endswith('.png') or x.endswith('.jpg')]
+        # 计算ascii图像的数量
+        num_of_pixel = num_of_file(globals.get_value('frames_of_pixel'))
+        globals.set_value('num_of_pixel', num_of_pixel)
 
-    #获取视频时长
-    para.num_of_pixel = num_of_file(frames_of_pixel)
+    # 获取视频时长
     get_dur()
 
-    #视频合帧及调速
+    # 视频合帧及调速
     integrate()
 
-    #获取音频
+    # 获取音频
     getAudio()
 
-    #视频音频合流
+    # 视频音频合流
     merge()
-
-
-
-
-
-
-
-
--- MAIN/command.py
+++ MAIN/command.py
@@ -1,69 +1,76 @@
-from parameterList import *
 import os
+from var import *
 
-#切换到项目目录
+globals.init()
+
+# 切换到项目目录
 
 def changeToProjectFold():
-    os.system("cd " + work_path)
+    os.system("cd " + globals.get_value('work_path'))
+    if not os.path.exists(globals.get_value('project_folder')):
+        os.mkdir(globals.get_value('project_folder'))
 
-#视频分帧
+
+# 视频分帧
 def getFrames():
     os.system("ffmpeg -i " +
-               name_of_original_vedio +
-               " -f image2" +
-               " -vf fps=fps=" +
-               str(frames) +
-               " -qscale:v 2 " +
-               frames_of_original +
-               "%05d.png"
-               )
+              globals.get_value('name_of_original_video') +
+              " -f image2" +
+              " -vf fps=fps=" +
+              str(globals.get_value('frames')) +
+              " -qscale:v 2 " +
+              globals.get_value('frames_of_original') +
+              "%05d.png"
+              )
 
-#视频合帧及调速
+
+# 视频合帧及调速
 def integrate():
-    if(pattern.PATTERN_ASCII == 1):
+    if (globals.get_value('PATTERN_ASCII')):
         os.system("ffmpeg -framerate " +
-                  str(round(1/(para.dur/para.num_of_ascii), 2)) +
+                  str(round(1 / (globals.get_value('dur') / globals.get_value('num_of_ascii')), 2)) +
                   " -f image2 -loop 1 -i " +
-                  frames_of_ascii +
+                  globals.get_value('frames_of_ascii') +
                   "%05d.png" +
-                  " -vcodec libx264 -pix_fmt yuv420p -s " + str(para.w) + "*" + str(para.h) + " -r " +
-                  str(frames) +
+                  " -vcodec libx264 -pix_fmt yuv420p -s " + str(globals.get_value('wide')) + "*" + str(globals.get_value('high')) + " -r " +
+                  str(globals.get_value('frames')) +
                   " -t " +
-                  str(para.dur) +
+                  str(globals.get_value('dur')) +
                   " -y " +
-                  name_of_no_speed_vedio
+                  globals.get_value('name_of_no_speed_video')
                   )
-    elif (pattern.PATTERN_PIXEL == 1):
+    elif (globals.get_value('PATTERN_PIXEL')):
         os.system("ffmpeg -framerate " +
-                  str(round(1/(para.dur/para.num_of_pixel), 2)) +
+                  str(round(1 / (globals.get_value('dur') / globals.get_value('num_of_pixel')), 2)) +
                   " -f image2 -loop 1 -i " +
-                  frames_of_pixel +
+                  globals.get_value('frames_of_pixel') +
                   "%05d.png" +
-                  " -vcodec libx264 -pix_fmt yuv420p -s " + str(para.w) + "*" + str(para.h) + " -r " +
-                  str(frames) +
+                  " -vcodec libx264 -pix_fmt yuv420p -s " + str(globals.get_value('wide')) + "*" + str(globals.get_value('high')) + " -r " +
+                  str(globals.get_value('frames')) +
                   " -t " +
-                  str(para.dur) +
+                  str(globals.get_value('dur')) +
                   " -y " +
-                  name_of_no_speed_vedio
+                  globals.get_value('name_of_no_speed_video')
                   )
 
-#获取音频
+
+# 获取音频
 def getAudio():
     os.system(
-              "ffmpeg -i " +
-              name_of_original_vedio +
-              " -c:a libmp3lame -q:a 2 " +
-              name_of_audio
-             )
+        "ffmpeg -i " +
+        globals.get_value('name_of_original_video') +
+        " -c:a libmp3lame -q:a 2 " +
+        globals.get_value('name_of_audio')
+    )
 
 
-#视频音频合流
+# 视频音频合流
 def merge():
     os.system(
-              "ffmpeg -i " +
-              name_of_no_speed_vedio +
-              " -i " +
-              name_of_audio +
-              " -c:v copy -map 0:v -map 1:a " +
-              name_of_final_vedio
-             )
+        "ffmpeg -i " +
+        globals.get_value('name_of_no_speed_video') +
+        " -i " +
+        globals.get_value('name_of_audio') +
+        " -c:v copy -map 0:v -map 1:a " +
+        globals.get_value('name_of_final_video')
+    )
